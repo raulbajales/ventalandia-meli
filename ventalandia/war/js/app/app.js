0526f5f126d25ca202c6a3a57cbd8d44c9a54e7b
raul.bajales <raul.bajales@gmail.com>
2012-09-02 17:24:36 -0300

    angular app bootstrap

--- ventalandia/war/js/app/app.js
+++ ventalandia/war/js/app/app.js
@@ -2,9 +2,10 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('vldApp', ['vldApp.filters', 'vldApp.services', 'vldApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/news', {templateUrl: '/partials/news.html', controller: NewsController});
+    $routeProvider.when('/customers', {templateUrl: '/partials/customers.html', controller: CustomersController});
+    $routeProvider.when('/products', {templateUrl: '/partials/products.html', controller: ProductsController});
+    $routeProvider.otherwise({redirectTo: '/news'});
   }]);
