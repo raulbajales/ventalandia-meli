20c90130778ae8f0dbc48f1964b9962a21225139
German Zanussi <gzanussi@gmail.com>
2012-09-18 10:10:34 -0300

    QuestionTransformerTest

--- ventalandia-test/src/com/ventalandia/domain/transformer/QuestionTransformerTest.java
+++ ventalandia-test/src/com/ventalandia/domain/transformer/QuestionTransformerTest.java
@@ -4,6 +4,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.ventalandia.domain.helper.QuestionHelper;
 import com.ventalandia.meli.api.notification.Question;
 
 public class QuestionTransformerTest {
@@ -12,7 +13,7 @@ public class QuestionTransformerTest {
     @Test
     public void testTransform(){
         
-        Question in = GSonBuilde;
+        Question in = QuestionHelper.create();
         Transformer<Question, com.ventalandia.domain.Question> questionTransformer = new QuestionTransformer();
         
         Assert.assertNotNull(questionTransformer.transform(in));
