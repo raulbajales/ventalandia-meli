7d1cb9d8334b307150ec2f22e2d1fea570ce380a
Matias Sulik <msulik@despegar.com>
2012-10-10 17:09:21 -0300

    cheking cookie

--- src/main/java/com/ventalandia/filter/ApiSecurityFilter.java
+++ src/main/java/com/ventalandia/filter/ApiSecurityFilter.java
@@ -1,11 +1,15 @@
 package com.ventalandia.filter;
 
+import java.net.URLDecoder;
+
 import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.inject.Singleton;
 import com.ventalandia.api.AuthServlet;
+import com.ventalandia.view.filter.WebappSecurityFilter;
 
 /**
  * 
@@ -34,16 +38,35 @@ public class ApiSecurityFilter extends AbstractSecurityFilter {
             throw new RuntimeException("Unable to handle a valid session", e);
         }
     }
-    
+
     protected String getVtdToken(HttpServletRequest request) {
         String hash = request.getHeader(X_VTD_TOKEN);
         if (hash != null && hash.length() > 0) {
             return hash;
         }
         else {
-            return null;
+            return getVtdTokenFromCookie(request);
         }
     }
 
+    protected String getVtdTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals(WebappSecurityFilter.VTD_TOKEN)) {
+                try {
+                    return URLDecoder.decode(cookie.getValue(), "UTF-8");
+                }
+                catch (Exception e) {
+                    throw new RuntimeException("Unable to get token from cookie", e);
+                }
+            }
+            else {
+                return null;
+            }
+        }
+        return null;
+    }
 
 }
