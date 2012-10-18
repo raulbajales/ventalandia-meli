5457d63fdad2bd44be0216df6f8fd4f05e9811fd
Matias Sulik <msulik@despegar.com>
2012-10-18 02:10:32 -0300

    adding answer for question (meli integration)

--- src/main/java/com/ventalandia/meli/service/MeliQuestionService.java
+++ src/main/java/com/ventalandia/meli/service/MeliQuestionService.java
@@ -1,6 +1,8 @@
 package com.ventalandia.meli.service;
 
 import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
 import com.ventalandia.framework.http.HttpRequestBuilder;
 import com.ventalandia.meli.api.notification.MeliNotification;
 import com.ventalandia.meli.api.notification.Question;
@@ -12,6 +14,9 @@ import com.ventalandia.meli.api.notification.Question;
  */
 public class MeliQuestionService extends AbstractMeliService {
 
+    @Inject
+    private Gson gson;
+
     public Question getQuestionByNotification(MeliNotification meliNotification) {
         String accessToken = AuthContext.getToken().getAccess_token();
         HttpRequestBuilder builder = this.createJsonGet().withPath(meliNotification.getResource()).addParam("access_token", accessToken);
@@ -21,4 +26,48 @@ public class MeliQuestionService extends AbstractMeliService {
         return this.parseEntity(httpResponse, Question.class);
     }
 
+    public void answer(long question_id, String text) {
+        String accessToken = AuthContext.getToken().getAccess_token();
+        HttpRequestBuilder builder = this.createJsonPost().withPath("answers") //
+                .addParam("access_token", accessToken) //
+                .body(this.gson.toJson(new AnswerBody(question_id, text)));
+
+        HTTPResponse httpResponse = this.execute(builder);
+
+        if (httpResponse.getResponseCode() != 200) {
+            throw new RuntimeException(new String(httpResponse.getContent()));
+        }
+    }
+
+    // setter and getter are unused on this class, they are here just for json
+    // serialization.
+    @SuppressWarnings("unused")
+    private class AnswerBody {
+
+        private long question_id;
+
+        private String text;
+
+        public AnswerBody(long question_id, String text) {
+            this.question_id = question_id;
+            this.text = text;
+        }
+
+        public long getQuestion_id() {
+            return question_id;
+        }
+
+        public void setQuestion_id(long question_id) {
+            this.question_id = question_id;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+    }
 }
