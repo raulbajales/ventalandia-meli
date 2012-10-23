549a2e0022c4d01a17431fc3f3fc943feb400e24
raul.bajales <raul.bajales@gmail.com>
2012-10-23 02:24:21 -0300

    FIX #68

--- src/main/webapp/js/app/app.js
+++ src/main/webapp/js/app/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('vldApp', ['vldApp.filters', 'vldApp.services', 'vldApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
+angular.module('vldApp', ['ngCookies', 'vldApp.filters', 'vldApp.services', 'vldApp.directives']).
+  config(function($routeProvider) {
     $routeProvider.when('/news', {templateUrl: '/partials/news.html', controller: NewsController});
     $routeProvider.when('/customers', {templateUrl: '/partials/customers.html', controller: CustomersController});
     $routeProvider.when('/products', {templateUrl: '/partials/products.html', controller: ProductsController});
     $routeProvider.otherwise({redirectTo: '/news'});
-  }]);
+  });
--- src/main/webapp/js/app/controllers.js
+++ src/main/webapp/js/app/controllers.js
@@ -2,9 +2,25 @@
 
 /* Controllers */
 
-function MiniProfileController($scope) {
-	$scope.firstName = "Fulgencio";
-	$scope.lastName = "Sarlanga";
+function MiniProfileController($scope, $cookies, $http) {
+	var repLevels = {
+		0: "grey",
+		1: "red",
+		2: "orange",
+		3: "yellow",
+		4: "lightgreen",
+		5: "green"
+	};
+	$http({method: "GET", url: "/api/users/me", headers: {"x-vtd-token": $cookies["vtd_token"]}}).success(function(data, status, headers, config) {
+		$scope.firstName = data.name;
+		$scope.lastName = data.surname;
+		var repColor = repLevels[data.sellerReputationLevel];
+		$(".reputation.badge").css("background-color", repColor);
+	}).error(function(data, status, headers, config) {
+		alert("error! " + JSON.stringify(data));
+	});
+	$scope.firstName = "...";
+	$scope.lastName = "...";
 }
 
 
