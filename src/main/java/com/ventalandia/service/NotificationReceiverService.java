31885510f33d03a133af51ad3e3fcbcab3effa8c
Matias Sulik <msulik@despegar.com>
2012-10-26 19:16:28 -0300

    Issue #65: adding logger for Item and Order Notification

--- src/main/java/com/ventalandia/service/NotificationReceiverService.java
+++ src/main/java/com/ventalandia/service/NotificationReceiverService.java
@@ -2,13 +2,16 @@ package com.ventalandia.service;
 
 import java.util.logging.Logger;
 
+import com.google.appengine.api.urlfetch.HTTPResponse;
 import com.google.inject.Inject;
 import com.ventalandia.domain.User;
 import com.ventalandia.domain.transformer.QuestionTransformer;
+import com.ventalandia.framework.http.HttpRequestBuilder;
 import com.ventalandia.meli.api.notification.MeliNotification;
 import com.ventalandia.meli.api.notification.Question;
 import com.ventalandia.meli.pesistence.QuestionRepository;
 import com.ventalandia.meli.service.AbstractMeliService;
+import com.ventalandia.meli.service.AuthContext;
 import com.ventalandia.meli.service.MeliQuestionService;
 
 /**
@@ -19,7 +22,7 @@ import com.ventalandia.meli.service.MeliQuestionService;
 public class NotificationReceiverService extends AbstractMeliService {
 
     private static final Logger LOGGER = Logger.getLogger(NotificationReceiverService.class.getName());
-    
+
     @Inject
     private QuestionTransformer questionTransformer;
 
@@ -31,7 +34,7 @@ public class NotificationReceiverService extends AbstractMeliService {
 
     @Inject
     private NewsFeedService newsFeedService;
-    
+
     @Inject
     private MeliQuestionService meliQuestionService;
 
@@ -63,13 +66,28 @@ public class NotificationReceiverService extends AbstractMeliService {
     }
 
     private void receiveOrder(MeliNotification meliNotification) {
-        // TODO code it
-        LOGGER.info("Receiving order notification ... Not supported yet.");
+        LOGGER.info("Receiving order notification : " + meliNotification.getResource());
+
+        HTTPResponse httpResponse = this.getResourceFromMeli(meliNotification);
+
+        LOGGER.info("Order Response Code: " + httpResponse.getResponseCode());
+        LOGGER.info("Order Response Content: " + new String(httpResponse.getContent()));
+    }
+
+    private HTTPResponse getResourceFromMeli(MeliNotification meliNotification) {
+        String accessToken = AuthContext.getToken().getAccess_token();
+        HttpRequestBuilder builder = this.createJsonGet().withPath(meliNotification.getResource()).addParam("access_token", accessToken);
+
+        return this.execute(builder);
     }
 
     private void receiveItem(MeliNotification meliNotification) {
-        // TODO code it
-        LOGGER.info("Receiving item notification ... Not supported yet.");
+        LOGGER.info("Receiving item notification : " + meliNotification.getResource());
+
+        HTTPResponse httpResponse = this.getResourceFromMeli(meliNotification);
+
+        LOGGER.info("Item Response Code: " + httpResponse.getResponseCode());
+        LOGGER.info("Item Response Content: " + new String(httpResponse.getContent()));
     }
 
 }
